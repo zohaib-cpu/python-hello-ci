a33a0132476410c56d56581ad7efa2c33db87549
Zohaib Irshad <zirshad817@gmail.com>
2026-02-17 19:34:49 +0500

    Fix app.py and test_app.py structure for CI

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello World from Python CI/CD 🚀"
+def main():
+    print("Hello World from Python CI/CD 🚀")
 
 if __name__ == "__main__":
-    app.run()
+    main()
