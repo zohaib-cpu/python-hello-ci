a3340d840b5a2e2ae4040c67af68a4036bd5e29b
Zohaib Irshad <zirshad817@gmail.com>
2026-02-17 19:22:12 +0500

    Add Python CI/CD pipeline

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-def main():
-    print("Hello World from Python CI/CD 🚀")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World from Python CI/CD 🚀"
 
 if __name__ == "__main__":
-    main()
+    app.run()
